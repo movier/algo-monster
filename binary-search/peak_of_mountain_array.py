8fac8d464026783ad8006a414d07e807e150c82a
Oliver <olimovier@gmail.com>
2022-10-08 16:17:17 +0000

    Peak of mountain array

--- binary-search/peak_of_mountain_array.py
+++ binary-search/peak_of_mountain_array.py
@@ -2,7 +2,17 @@ from typing import List
 
 def peak_of_mountain_array(arr: List[int]) -> int:
     # WRITE YOUR BRILLIANT CODE HERE
-    return 0
+    left, right = 0, len(arr) - 1
+    mid = 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+            return mid
+        elif arr[mid] < arr[mid - 1]:
+            right = mid - 1
+        elif arr[mid] < arr[mid + 1]:
+            left = mid + 1
+    return mid
 
 if __name__ == '__main__':
     input = '30 40 50 10 20'
