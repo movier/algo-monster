776f5993cf129aaa517494e96f7499a405dd1d0c
Oliver <olimovier@gmail.com>
2022-10-13 07:13:29 +0000

    Work on letter combination

---
+++ depth-first-search/letter_combination.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def letter_combination(n: int) -> List[str]:
+    # WRITE YOUR BRILLIANT CODE HERE
+    return []
+
+if __name__ == '__main__':
+    input = '1'
+    n = int(input)
+    res = letter_combination(n)
+    for line in sorted(res):
+        print(line)
