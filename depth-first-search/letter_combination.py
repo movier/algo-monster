2f882621511f21a6aaf0944e082f170a231aa1a8
Oliver <olimovier@gmail.com>
2022-10-14 09:03:52 +0000

    Letter combination

--- depth-first-search/letter_combination.py
+++ depth-first-search/letter_combination.py
@@ -2,10 +2,21 @@ from typing import List
 
 def letter_combination(n: int) -> List[str]:
     # WRITE YOUR BRILLIANT CODE HERE
-    return []
+    def dfs(start_index, path):
+        if start_index == n:
+            res.append(''.join(path))
+            return
+        for letter in ['a', 'b']:
+            path.append(letter)
+            dfs(start_index + 1, path)
+            path.pop()
+    
+    res = []
+    dfs(0, [])
+    return res
 
 if __name__ == '__main__':
-    input = '1'
+    input = '6'
     n = int(input)
     res = letter_combination(n)
     for line in sorted(res):
