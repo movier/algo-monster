73673db5f42111da4a8e5ca3bfe6a657b3a366e3
Oliver <olimovier@gmail.com>
2022-10-14 09:05:48 +0000

    Work on permutations

---
+++ depth-first-search/permutations.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def permutations(letters: str) -> List[str]:
+    # WRITE YOUR BRILLIANT CODE HERE
+    return []
+
+if __name__ == '__main__':
+    input = 'ab'
+    letters = input
+    res = permutations(letters)
+    for line in sorted(res):
+        print(line)
