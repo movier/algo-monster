116d78e4b2ba736978036d649203ac16762bdcce
Oliver <olimovier@gmail.com>
2022-10-14 14:50:47 +0000

    Permutations

--- depth-first-search/permutations.py
+++ depth-first-search/permutations.py
@@ -2,10 +2,20 @@ from typing import List
 
 def permutations(letters: str) -> List[str]:
     # WRITE YOUR BRILLIANT CODE HERE
-    return []
+    def dfs(letters, path):
+        if len(letters) == 0:
+            res.append(''.join(path))
+            return
+        for l in letters:
+            path.append(l)
+            dfs(list(filter(lambda x: x != l, letters)), path)
+            path.pop()
+    res = []
+    dfs(letters, [])
+    return res
 
 if __name__ == '__main__':
-    input = 'ab'
+    input = 'abcd'
     letters = input
     res = permutations(letters)
     for line in sorted(res):
