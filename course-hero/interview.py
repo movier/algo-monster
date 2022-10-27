94b2f3e48c80f80a5c3333a9179a4738c4b3e36d
Oliver <olimovier@gmail.com>
2022-10-26 18:54:18 -0700

    Improve my answer

--- course-hero/interview.py
+++ course-hero/interview.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 
 class Course:
@@ -10,30 +11,34 @@ class Course:
     def __str__(self):
         return f'Department: {self.department}, Course Number: {self.course_number}, Year: {self.year}, Semester: {self.semester}'
 
+SEMESTER = {
+    'f': 'Fall',
+    'w': 'Winter',
+    's': 'Spring',
+    'su': 'Summer'
+}
+
 def course_string_to_object_step_by_step(course_str):
-    split_course_str_by_space = course_str.split(' ')
+    
     department_course_str, semester_year_str = '', ''
+    split_course_str_by_space = course_str.split(' ')
     for idx, x in enumerate(split_course_str_by_space):
         if x[-1].isdigit():
             department_course_str = ' '.join(split_course_str_by_space[0:idx + 1])
             semester_year_str = ' '.join(split_course_str_by_space[idx + 1:])
             break
-    print('department_course_str', department_course_str)
-    print('semester_year_str', semester_year_str)
-
-    split_department_course_str = re.split('-| |:', department_course_str)
+    
     department, course = '', ''
+    split_department_course_str = re.split('-| |:', department_course_str)
     if len(split_department_course_str) > 1:
         department = split_department_course_str[0]
         course = split_department_course_str[1]
     else:
         department = department_course_str.rstrip('0123456789')
         course = department_course_str[len(department):]
-    print('department', department)
-    print('course', course)
 
-    split_semester_year_str = semester_year_str.split(' ')
     semester, year = '', ''
+    split_semester_year_str = semester_year_str.split(' ')
     if (len(split_semester_year_str) > 1):
         if split_semester_year_str[0].isdigit():
             year = split_semester_year_str[0]
@@ -49,9 +54,19 @@ def course_string_to_object_step_by_step(course_str):
             semester = semester_year_str.rstrip('0123456789')
             year  = semester_year_str[len(semester):]
     
-    print('semester', semester)
-    print('year', year)
+    if len(year) == 2:
+        year = datetime.datetime.strptime(year,'%y').strftime('%Y')
+    
+    if len(semester) <= 2:
+        semester = SEMESTER[semester.lower()]
+    else:
+        semester = semester.capitalize()
+
+    course = Course(department, course, year, semester)
+    print(course)
 
 
 if __name__ == '__main__':
-    course_string_to_object_step_by_step('CS 111 F2016')
+    course_string_to_object_step_by_step('CS:111 16 fall')
+    course_string_to_object_step_by_step('CS-111 Fall 2016')
+    course_string_to_object_step_by_step('CS111 F2016')
