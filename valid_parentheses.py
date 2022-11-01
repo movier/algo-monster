00922bd6b5e0d8ed6299b3ad4e65634b36c32bf0
Oliver <olimovier@gmail.com>
2022-10-31 17:28:09 -0700

    Improve solution

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,6 +1,6 @@
+open_brackets = ['(', '{', '[']
+close_brackets = [')', '}', ']']
 def is_valid_parentheses(s):
-    open_brackets = ['(', '{', '[']
-    close_brackets = [')', '}', ']']
     open_brackets_stack = []
     for i in s:
         if i in open_brackets:
@@ -11,12 +11,12 @@ def is_valid_parentheses(s):
             idx = close_brackets.index(i)
             if open_brackets[idx] != open_brackets_stack.pop():
                 return False
-    
+
     if len(open_brackets_stack) == 0:
       return True
     return False
 
-print(is_valid_parentheses('()'))
+print(is_valid_parentheses('('))
 print(is_valid_parentheses('()[]{}'))
 print(is_valid_parentheses('(]'))
 print(is_valid_parentheses('({[]})'))
